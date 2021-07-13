8990c9349faa4147c6483db2507aed8a1467166a
[Sona] <[sona16061999@gmail.com]>
2021-07-13 19:04:24 +0530

    [Sona] regex pattern is created to check pincode contains space is valid or not

--- Regex/Program.cs
+++ Regex/Program.cs
@@ -8,7 +8,7 @@ namespace RegexExpression
         {
             Console.WriteLine("Welcome to Regex Expression Programs!");
             Pincode pin = new Pincode();
-            string[] pincode = { "400088", "A400088", "400088B"};
+            string[] pincode = { "400088", "A400088", "400088B", "400 088" };
             for (int i = 0; i < pincode.Length; i++)
             {
                 pin.Check(pincode[i]);
